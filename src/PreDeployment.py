77c05c484c9fdedaa5afe3a002554b5d97f0606c
Guilherme Elias <guilherme.elias@gmail.com>
2011-07-15 01:14:27 -0300

    mapped all pre-deployment stepd and it's testings

--- src/PreDeployment.py
+++ src/PreDeployment.py
@@ -1,15 +1,34 @@
-import os
-
 class PreDeployment(object):
 
-    def __init__(self, app_name):
+    def __init__(self, app_name, commands):
         self.app_name = app_name
+        self.commands = commands
 
+    def doDownloadOfAppToDeployment(self):
+        download_result = self.commands.download_app(self.app_name)
+        return download_result
+
+    
+    def buildFoldersOnServersToUploadAppFile(self):
+        folders_built_result = self.commands.build_folders_on_servers()
+        return folders_built_result
+
+    
+    def uploadAppFileToTheServers(self):
+        upload_result = self.commands.upload_app_to_servers(self.app_name, self.commands.app_file_folder_name())
+        return upload_result
+
+    
     def start(self):
-        os.system('/tmp/script_ftp.sh %s' % self.app_name)
-        f=os.popen("ftp_script.log")
-        output = ""
-        for i in f.readlines():
-            output += i
-        return output
+        download_result = self.doDownloadOfAppToDeployment()
+        build_folders_result = self.buildFoldersOnServersToUploadAppFile()
+        upload_app_result = self.uploadAppFileToTheServers()
+        
+        return download_result == build_folders_result == upload_app_result
+    
+    
+    
+    
+    
+    
         
